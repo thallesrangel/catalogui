2d8849783747cbdbe9cf1c9ab963de5a034b0c74
Thalles <rangelthr@gmail.com>
2022-01-21 00:00:20 -0300

    add service state

---
+++ app/Repositories/StateRepository.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\State;
+
+class StateRepository
+{
+    protected $states;
+
+    public function __construct(State $states)
+    {
+        $this->states = $states;
+    }
+
+    public function get()
+    {
+  
+        return $this->states->get();
+    }
+}
