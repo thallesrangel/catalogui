6d754c47d57cc8874fd746585d1951e21265b127
Thalles <rangelthr@gmail.com>
2022-01-25 10:59:21 -0300

    ajustes

--- resources/views/dashboard/includes/navbar.blade.php
+++ resources/views/dashboard/includes/navbar.blade.php
@@ -3,12 +3,12 @@
         <img class="img-fluid" src="{{ asset('img/logo.png')}}" width="140" height="24">
         <span class="txt-green">Beta</span>
     </a>
-    <ul class="nav">
+    <ul class="nav nav-dash-admin">
         <li class="nav-item">
-            <a class="nav-link" href="{{ route('dashboard') }}">Início</a>
+            <a class="nav-link" href="{{ route('dashboard') }}" title="Início"><i class="bi bi-house"></i></a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="{{ route('logout') }}">Sair</a>
+            <a class="nav-link" href="{{ route('logout') }}" title="Sair"><i class="bi bi-box-arrow-right"></i></a>
         </li>
         <li class="nav-item">
             <a class="nav-link btn btn-primary" href="{{ route('announcement.create') }}">Anunciar</a>
